cbd2c4516e7f569c840fc9a90ac8cd28b2bb5a4a
weliv <weliv@10.0.0.3>
2021-11-24 09:58:10 -0400

    primero metodos base

--- ProyectoFinal/src/logico/Tienda.java
+++ ProyectoFinal/src/logico/Tienda.java
@@ -1,15 +1,22 @@
 package logico;
 
 import java.util.ArrayList;
+import java.util.Date;
+
 public class Tienda {
+private int cantClientesReal;
 ArrayList<Combos>miscombos;
 ArrayList<Factura>misfacturas;
 ArrayList<Cliente>misclientes;
+ArrayList<OrdenCompra>mis_orden;
+
 public Tienda(ArrayList<Combos> miscombos, ArrayList<Factura> misfacturas, ArrayList<Cliente> misclientes) {
 	super();
 	this.miscombos =  new ArrayList<Combos>();
 	this.misfacturas =  new ArrayList<Factura>();
 	this.misclientes =  new ArrayList<Cliente>();
+	this.mis_orden = new ArrayList<OrdenCompra>();
+	this.cantClientesReal=0;
 }
 public ArrayList<Combos> getMiscombos() {
 	return miscombos;
@@ -29,4 +36,44 @@ public void setMisfacturas(ArrayList<Factura> misfacturas) {
 public void setMisclientes(ArrayList<Cliente> misclientes) {
 	this.misclientes = misclientes;
 }
+public void crearCliente(String nombre, String direccion ,String telefono ,String rnc ,String cedula ,float limiteCredito ,float cuentasxCobrar) {
+	Cliente primer = new Cliente( nombre,direccion,telefono,rnc,cedula,limiteCredito , cuentasxCobrar);
+	misclientes.add(primer);
+	cantClientesReal= cantClientesReal+1;
 }
+public Cliente buscarCliente(String Cedula) {
+	Cliente aux = null;
+	boolean encontrado = false;
+	int buscador=0;
+	
+	while(!encontrado && buscador<cantClientesReal ) {
+		if(misclientes.get(buscador).getCedula().equalsIgnoreCase(Cedula)) {
+			encontrado=true;
+			aux= misclientes.get(buscador);
+		}
+		buscador++;
+	}
+	
+	return aux;
+}/*
+public Cliente buscarComponente(int numeroserie) {
+	Cliente aux = null;
+	boolean encontrado = false;
+	int buscador=0;
+	
+	while(!encontrado && buscador<cantClientesReal ) {
+		if(misclientes.get(buscador).getequalsIgnoreCase(Cedula)) {
+			encontrado=true;
+			aux= misclientes.get(buscador);
+		}
+		buscador++;
+	}
+	
+	return aux;
+}*/
+public void CrearOrdendecompra(String codigo, Date fecha, String estado, int cantidad, Componente compo, float preciounitario,
+		Proveedor proveedor, String tipodepago) {
+	OrdenCompra orde = new OrdenCompra(codigo, fecha, estado, cantidad, compo, preciounitario, proveedor, tipodepago);
+	mis_orden.add(orde);
+}
+};
