f88589243544b4217111885050e6b778fba7ee8c
Alamc <Alamc@AlamPc>
2021-11-26 11:47:43 -0500

    Metodo para cantidad en las listas

--- ProyectoFinal/src/Visual/OrdenCompraList.java
+++ ProyectoFinal/src/Visual/OrdenCompraList.java
@@ -92,11 +92,19 @@ public class OrdenCompraList extends JDialog {
 				btnEstado.setEnabled(false);
 				btnEstado.addActionListener(new ActionListener() {
 					public void actionPerformed(ActionEvent e) {
+						if(selected.getEstado().equalsIgnoreCase("Proceso"))
+						{
+							selected.setEstado("Confirmado");
+							btnEstado.setEnabled(false);
+							//Total(cantidad*preciunitario) se le sume a la cuenta por pagar 
+							selected.getProveedor().setCuentaxpagar((selected.getCantidad()*selected.getPreciounitario())+ selected1.getProveedor().getCuentaxpagar());
+						}
 						if(selected.getEstado().equalsIgnoreCase("Confirmado"))
 						{
 							selected.setEstado("Entregado");
 							btnEstado.setEnabled(false);
 							selected1.setCantidadDisponible(selected.getCantidad()+ selected1.getCantidadDisponible());
+							selected.getProveedor().setCuentaxpagar(selected1.getProveedor().getCuentaxpagar()-(selected.getCantidad()*selected.getPreciounitario()));
 						}
 						if(selected.getEstado().equalsIgnoreCase("Entregado"))
 						{
