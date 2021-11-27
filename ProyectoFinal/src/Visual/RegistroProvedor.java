220731af474bd5208c38a925f665284e889053a7
Alamc <Alamc@AlamPc>
2021-11-27 11:20:42 -0500

    Solo dise;o de componente

---
+++ ProyectoFinal/src/Visual/RegistroProvedor.java
@@ -0,0 +1,191 @@
+package Visual;
+
+import java.awt.BorderLayout;
+import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JDialog;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+import javax.swing.border.EmptyBorder;
+import javax.swing.border.TitledBorder;
+
+import logico.Cliente;
+import logico.Proveedor;
+import logico.Tienda;
+import javax.swing.UIManager;
+import java.awt.Color;
+
+public class RegistroProvedor extends JDialog {
+
+	private final JPanel contentPanel = new JPanel();
+	private JButton RegistrarButton;
+	private JTextField textField_Nombre;
+	private JTextField textField_direccion;
+	private JTextField textField_RNC;
+	private JTextField textField_Telefono;
+	private Proveedor cli = null;
+	private JTextField textField_buscarprovedor;
+	private JButton SalirButton;
+	private JButton btnBuscarProveedor;
+
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		try {
+			RegistroProvedor dialog = new RegistroProvedor();
+			dialog.setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
+			dialog.setVisible(true);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * Create the dialog.
+	 */
+	public RegistroProvedor() {
+		setTitle("Registro Proveedor");
+		setBounds(100, 100, 610, 420);
+		getContentPane().setLayout(new BorderLayout());
+		contentPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
+		getContentPane().add(contentPanel, BorderLayout.CENTER);
+		contentPanel.setLayout(null);
+		{
+			JPanel panel = new JPanel();
+			panel.setBorder(new TitledBorder(null, "", TitledBorder.LEADING, TitledBorder.TOP, null, null));
+			panel.setBounds(10, 95, 572, 237);
+			contentPanel.add(panel);
+			panel.setLayout(null);
+			
+			textField_Nombre = new JTextField();
+			textField_Nombre.setEnabled(false);
+			textField_Nombre.setColumns(10);
+			textField_Nombre.setBounds(10, 46, 217, 20);
+			panel.add(textField_Nombre);
+			
+			textField_direccion = new JTextField();
+			textField_direccion.setEnabled(false);
+			textField_direccion.setColumns(10);
+			textField_direccion.setBounds(10, 107, 217, 20);
+			panel.add(textField_direccion);
+			
+			textField_RNC = new JTextField();
+			textField_RNC.setEnabled(false);
+			textField_RNC.setColumns(10);
+			textField_RNC.setBounds(325, 107, 217, 20);
+			panel.add(textField_RNC);
+			
+			textField_Telefono = new JTextField();
+			textField_Telefono.setEnabled(false);
+			textField_Telefono.setColumns(10);
+			textField_Telefono.setBounds(325, 46, 217, 20);
+			panel.add(textField_Telefono);
+			
+			JLabel lblNewLabel = new JLabel("Nombre");
+			lblNewLabel.setBounds(86, 21, 46, 14);
+			panel.add(lblNewLabel);
+			
+			JLabel lblDireccion = new JLabel("Direccion");
+			lblDireccion.setBounds(86, 82, 58, 14);
+			panel.add(lblDireccion);
+			
+			JLabel lblRnc = new JLabel("RNC");
+			lblRnc.setBounds(415, 93, 46, 14);
+			panel.add(lblRnc);
+			
+			JLabel lblTelefono = new JLabel("Telefono");
+			lblTelefono.setBounds(415, 21, 58, 14);
+			panel.add(lblTelefono);
+			
+			JLabel lblNewLabel_1 = new JLabel("Componente");
+			lblNewLabel_1.setBounds(86, 163, 85, 14);
+			panel.add(lblNewLabel_1);
+		}
+		
+		JPanel panel = new JPanel();
+		panel.setBorder(new TitledBorder(UIManager.getBorder("TitledBorder.border"), "Agregar RNC", TitledBorder.LEADING, TitledBorder.TOP, null, new Color(0, 0, 0)));
+		panel.setBounds(10, 11, 573, 73);
+		contentPanel.add(panel);
+		panel.setLayout(null);
+		
+		textField_buscarprovedor = new JTextField();
+		textField_buscarprovedor.setColumns(10);
+		textField_buscarprovedor.setBounds(166, 11, 217, 20);
+		panel.add(textField_buscarprovedor);
+		
+		btnBuscarProveedor = new JButton("Buscar Proveedor");
+		btnBuscarProveedor.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				cli = Tienda.getInstance().buscarProveedor(textField_buscarprovedor.getText());
+				if (cli == null) {
+					textField_Nombre.setEnabled(true);
+					textField_direccion.setEnabled(true);
+					textField_RNC.setEnabled(true);
+					textField_Telefono.setEnabled(true);
+		    		 JOptionPane.showMessageDialog(null, "ES UN PROVEEDOR NUEVO POR FAVOR LLENE LOS CAMPOS Y PROCEDA A COMPRAR");
+		    	}
+				if(cli != null){
+		    		textField_Nombre.setText(cli.getNombre());
+		    		textField_direccion.setText(cli.getDireccion());
+		    		textField_RNC.setText(cli.getRnc());
+		    		textField_Telefono.setText(cli.getTelefono());
+		    		 JOptionPane.showMessageDialog(null,"GRACIAS POR VOLVER");
+		    		
+		    		 textField_Nombre.setEnabled(false);
+						textField_direccion.setEnabled(false);
+						textField_RNC.setEnabled(false);
+						textField_Telefono.setEnabled(false);
+			}
+			}
+		});
+		btnBuscarProveedor.setBounds(220, 42, 130, 23);
+		panel.add(btnBuscarProveedor);
+		{
+			JPanel buttonPane = new JPanel();
+			buttonPane.setBorder(new TitledBorder(null, "", TitledBorder.LEADING, TitledBorder.TOP, null, null));
+			buttonPane.setLayout(new FlowLayout(FlowLayout.RIGHT));
+			getContentPane().add(buttonPane, BorderLayout.SOUTH);
+			{
+				RegistrarButton = new JButton("Registrar");
+				RegistrarButton.addActionListener(new ActionListener() {
+					public void actionPerformed(ActionEvent e) {
+						Proveedor a = new Proveedor(textField_Nombre.getText(),textField_RNC.getText(),textField_Telefono.getText(),textField_direccion.getText());
+						Tienda.getInstance().insertarProveedor(a);
+						 JOptionPane.showMessageDialog(null,"Registro Satisfactorio","Informacion",JOptionPane.INFORMATION_MESSAGE);
+		                 clean();
+		                 textField_Nombre.setEnabled(false);
+							textField_direccion.setEnabled(false);
+							textField_RNC.setEnabled(false);
+							textField_Telefono.setEnabled(false);
+					}
+
+					private void clean() {
+						textField_Nombre.setText("");
+			    		textField_direccion.setText("");
+			    		textField_RNC.setText("");
+			    		textField_Telefono.setText("");
+					}
+				});
+				RegistrarButton.setActionCommand("OK");
+				buttonPane.add(RegistrarButton);
+				getRootPane().setDefaultButton(RegistrarButton);
+			}
+			{
+				SalirButton = new JButton("Salir");
+				SalirButton.addActionListener(new ActionListener() {
+					public void actionPerformed(ActionEvent e) {
+					}
+				});
+				SalirButton.setActionCommand("Cancel");
+				buttonPane.add(SalirButton);
+			}
+		}
+	}
+}
--- ProyectoFinal/src/Visual/componenteNuevo.java
+++ ProyectoFinal/src/Visual/componenteNuevo.java
@@ -11,6 +11,7 @@ import javax.swing.border.TitledBorder;
 
 import logico.Componente;
 import logico.Proveedor;
+import logico.TarjetaMadre;
 import logico.Tienda;
 
 import javax.swing.JLabel;
@@ -26,28 +27,41 @@ import java.awt.event.ActionEvent;
 import javax.swing.SpinnerNumberModel;
 import javax.swing.JComboBox;
 import javax.swing.DefaultComboBoxModel;
+import javax.swing.UIManager;
+import java.awt.Color;
+import java.awt.Rectangle;
 
 public class componenteNuevo extends JDialog {
 
 	private final JPanel contentPanel = new JPanel();
-	private JTextField RadioEsfera;
-	private JTextField RadioCilindro;
-	private JTextField Longitud;
-	private JTextField RadioCilindrohueco;
-	private JTextField Longitud2;
-	private JTextField RadioInterior;
-	private JRadioButton rCilindro;
-	private JRadioButton rCilindrohueco;
-	private JPanel panel3;
+	private JTextField TextFieldMarca;
+	private JTextField textFieldMarca;
+	private JTextField textFieldModelo;
+	private JTextField textfieldMarca;
+	private JTextField textfieldTipoconector;
+	private JTextField textfieldModelo;
+	private JRadioButton rDisco;
+	private JRadioButton rTarjetaMadre;
+	private JPanel panelRam;
 	private JPanel panel2;
 	private JPanel panel;
-	private JPanel panel4;
-	private JPanel panel5;
-	private JRadioButton rEsfera;
+	private JPanel panelDisco;
+	private JPanel panelMadre;
+	private JRadioButton rRam;
 	private JSpinner spnprecio;
 	private JSpinner spncantidad;
 	private JSpinner spnMinima;
-	private JTextField textField;
+	private JTextField textFieldNombreComponente;
+	private JTextField textFieldTipoConexion;
+	private JComboBox cbxCapacidadAlmacenamiento;
+	private JTextField textFieldTipoMemoria;
+	private JComboBox comboBoxCantidadMemoria;
+	private JTextField textFieldMarca1;
+	private JTextField textFieldModelo2;
+	private JTextField textFieldTipoconetor;
+	private JComboBox cbxVelocidad;
+	private JPanel panelProcesador;
+	private JRadioButton rProcesador;
 
 	/**
 	 * Launch the application.
@@ -72,60 +86,60 @@ public class componenteNuevo extends JDialog {
 		
 		setResizable(false);
 		setTitle(".:. INTRODUCION DE NUEVO COMPONENTE .:.");
-		setBounds(100, 100, 530, 472);
+		setBounds(100, 100, 530, 433);
 		setLocationRelativeTo(null);
 		getContentPane().setLayout(new BorderLayout());
-		contentPanel.setBorder(new TitledBorder(null, "Introcion de Componente", TitledBorder.LEADING, TitledBorder.TOP, null, null));
+		contentPanel.setBorder(new TitledBorder(UIManager.getBorder("TitledBorder.border"), "Introduccion de Componente", TitledBorder.LEADING, TitledBorder.TOP, null, new Color(0, 0, 0)));
 		getContentPane().add(contentPanel, BorderLayout.CENTER);
 		contentPanel.setLayout(null);
 		{
-			panel5 = new JPanel();
-			panel5.setLayout(null);
-			panel5.setBorder(new TitledBorder(null, "", TitledBorder.LEADING, TitledBorder.TOP, null, null));
-			panel5.setBounds(6, 229, 508, 119);
-			contentPanel.add(panel5);
+			panelMadre = new JPanel();
+			panelMadre.setLayout(null);
+			panelMadre.setBorder(new TitledBorder(null, "", TitledBorder.LEADING, TitledBorder.TOP, null, null));
+			panelMadre.setBounds(6, 229, 508, 119);
+			contentPanel.add(panelMadre);
 			{
 				JLabel lblMarca = new JLabel("Marca");
 				lblMarca.setBounds(10, 11, 106, 14);
-				panel5.add(lblMarca);
+				panelMadre.add(lblMarca);
 			}
 			{
-				RadioCilindrohueco = new JTextField();
-				RadioCilindrohueco.setColumns(10);
-				RadioCilindrohueco.setBounds(10, 36, 136, 20);
-				panel5.add(RadioCilindrohueco);
+				textfieldMarca = new JTextField();
+				textfieldMarca.setColumns(10);
+				textfieldMarca.setBounds(10, 36, 136, 20);
+				panelMadre.add(textfieldMarca);
 			}
 			{
 				JLabel lblTipoDeConector = new JLabel("Tipo de conector");
 				lblTipoDeConector.setBounds(10, 71, 106, 14);
-				panel5.add(lblTipoDeConector);
+				panelMadre.add(lblTipoDeConector);
 			}
 			{
-				Longitud2 = new JTextField();
-				Longitud2.setColumns(10);
-				Longitud2.setBounds(10, 88, 136, 20);
-				panel5.add(Longitud2);
+				textfieldTipoconector = new JTextField();
+				textfieldTipoconector.setColumns(10);
+				textfieldTipoconector.setBounds(10, 88, 136, 20);
+				panelMadre.add(textfieldTipoconector);
 			}
 			{
-				RadioInterior = new JTextField();
-				RadioInterior.setColumns(10);
-				RadioInterior.setBounds(206, 36, 136, 20);
-				panel5.add(RadioInterior);
+				textfieldModelo = new JTextField();
+				textfieldModelo.setColumns(10);
+				textfieldModelo.setBounds(206, 36, 136, 20);
+				panelMadre.add(textfieldModelo);
 			}
 			{
 				JLabel lblRadioInterior = new JLabel("Modelo");
 				lblRadioInterior.setBounds(206, 11, 115, 14);
-				panel5.add(lblRadioInterior);
+				panelMadre.add(lblRadioInterior);
 			}
 			
 			JLabel lblTipoDeRam = new JLabel("Tipo de Ram");
 			lblTipoDeRam.setBounds(206, 71, 106, 14);
-			panel5.add(lblTipoDeRam);
+			panelMadre.add(lblTipoDeRam);
 			
-			JComboBox comboBox = new JComboBox();
-			comboBox.setModel(new DefaultComboBoxModel(new String[] {"<<Seleccionar>>", "DR1", "DR2", "DR3", "DR4", "DR5"}));
-			comboBox.setBounds(206, 88, 136, 20);
-			panel5.add(comboBox);
+			JComboBox cbxTipoRam = new JComboBox();
+			cbxTipoRam.setModel(new DefaultComboBoxModel(new String[] {"<<Seleccionar>>", "DR1", "DR2", "DR3", "DR4", "DR5"}));
+			cbxTipoRam.setBounds(206, 88, 136, 20);
+			panelMadre.add(cbxTipoRam);
 		}
 		{
 			panel = new JPanel();
@@ -173,39 +187,57 @@ public class componenteNuevo extends JDialog {
 			lblNombreDelComponente.setBounds(10, 11, 155, 15);
 			panel.add(lblNombreDelComponente);
 			
-			textField = new JTextField();
-			textField.setBounds(10, 37, 488, 20);
-			panel.add(textField);
-			textField.setColumns(10);
+			textFieldNombreComponente = new JTextField();
+			textFieldNombreComponente.setBounds(10, 37, 488, 20);
+			panel.add(textFieldNombreComponente);
+			textFieldNombreComponente.setColumns(10);
 		}
 		{
-			panel4 = new JPanel();
-			panel4.setLayout(null);
-			panel4.setBorder(new TitledBorder(null, "", TitledBorder.LEADING, TitledBorder.TOP, null, null));
-			panel4.setBounds(6, 229, 422, 119);
-			contentPanel.add(panel4);
+			panelDisco = new JPanel();
+			panelDisco.setLayout(null);
+			panelDisco.setBorder(new TitledBorder(null, "", TitledBorder.LEADING, TitledBorder.TOP, null, null));
+			panelDisco.setBounds(6, 229, 508, 119);
+			contentPanel.add(panelDisco);
 			{
-				JLabel lblRadioCilindro = new JLabel("Radio Cilindro");
-				lblRadioCilindro.setBounds(10, 11, 110, 14);
-				panel4.add(lblRadioCilindro);
+				JLabel lblMarca = new JLabel("Marca");
+				lblMarca.setBounds(10, 11, 110, 14);
+				panelDisco.add(lblMarca);
 			}
 			{
-				RadioCilindro = new JTextField();
-				RadioCilindro.setColumns(10);
-				RadioCilindro.setBounds(10, 36, 136, 20);
-				panel4.add(RadioCilindro);
+				textFieldMarca = new JTextField();
+				textFieldMarca.setColumns(10);
+				textFieldMarca.setBounds(10, 36, 136, 20);
+				panelDisco.add(textFieldMarca);
 			}
 			{
-				JLabel lblLongitud = new JLabel("Longitud");
-				lblLongitud.setBounds(206, 11, 102, 14);
-				panel4.add(lblLongitud);
+				JLabel lblModelo = new JLabel("Modelo");
+				lblModelo.setBounds(206, 11, 102, 14);
+				panelDisco.add(lblModelo);
 			}
 			{
-				Longitud = new JTextField();
-				Longitud.setColumns(10);
-				Longitud.setBounds(206, 36, 136, 20);
-				panel4.add(Longitud);
+				textFieldModelo = new JTextField();
+				textFieldModelo.setColumns(10);
+				textFieldModelo.setBounds(206, 36, 136, 20);
+				panelDisco.add(textFieldModelo);
 			}
+			
+			JLabel lblCapacidadAlmacenamiento = new JLabel("Tipo Conexion");
+			lblCapacidadAlmacenamiento.setBounds(10, 71, 106, 14);
+			panelDisco.add(lblCapacidadAlmacenamiento);
+			
+			textFieldTipoConexion = new JTextField();
+			textFieldTipoConexion.setBounds(10, 88, 136, 20);
+			panelDisco.add(textFieldTipoConexion);
+			textFieldTipoConexion.setColumns(10);
+			
+			cbxCapacidadAlmacenamiento = new JComboBox();
+			cbxCapacidadAlmacenamiento.setModel(new DefaultComboBoxModel(new String[] {"<<Seleccionar>>", "2TB", "4TB", "6TB", "8TB", "10TB"}));
+			cbxCapacidadAlmacenamiento.setBounds(206, 88, 136, 20);
+			panelDisco.add(cbxCapacidadAlmacenamiento);
+			
+			JLabel lblNewLabel_1 = new JLabel("Almacenamiento");
+			lblNewLabel_1.setBounds(206, 71, 155, 14);
+			panelDisco.add(lblNewLabel_1);
 		}
 		{
 			panel2 = new JPanel();
@@ -214,82 +246,154 @@ public class componenteNuevo extends JDialog {
 			contentPanel.add(panel2);
 			panel2.setLayout(null);
 			{
-				rEsfera = new JRadioButton("Ram");
-				rEsfera.addActionListener(new ActionListener() {
+				rRam = new JRadioButton("Ram");
+				rRam.addActionListener(new ActionListener() {
 					public void actionPerformed(ActionEvent e) {
-					panel3.setVisible(true);
-						panel4.setVisible(false);
-						panel5.setVisible(false);
-						rEsfera.setSelected(true);
-						rCilindro.setSelected(false);
-						rCilindrohueco.setSelected(false);	
+					panelRam.setVisible(true);
+						panelDisco.setVisible(false);
+						panelMadre.setVisible(false);
+						panelProcesador.setVisible(false);
+						rRam.setSelected(true);
+						rDisco.setSelected(false);
+						rProcesador.setSelected(false);
+						rTarjetaMadre.setSelected(false);	
 					}
 					
 				});
-				rEsfera.setBounds(126, 7, 91, 23);
-				panel2.add(rEsfera);
+				rRam.setBounds(126, 7, 91, 23);
+				panel2.add(rRam);
 			}
 			{
-				rCilindro = new JRadioButton("Disco");
-				rCilindro.addActionListener(new ActionListener() {
+				rDisco = new JRadioButton("Disco");
+				rDisco.addActionListener(new ActionListener() {
 					public void actionPerformed(ActionEvent e) {
-						panel4.setVisible(true);
-						panel3.setVisible(false);
-						panel5.setVisible(false);
-						rEsfera.setSelected(false);
-						rCilindro.setSelected(true);
-						rCilindrohueco.setSelected(false);	
+						panelDisco.setVisible(true);
+						panelRam.setVisible(false);
+						panelMadre.setVisible(false);
+						panelProcesador.setVisible(false);
+						rRam.setSelected(false);
+						rDisco.setSelected(true);
+						rTarjetaMadre.setSelected(false);
+						rProcesador.setSelected(false);
 					}
 				});
-				rCilindro.setBounds(219, 7, 98, 23);
-				panel2.add(rCilindro);
+				rDisco.setBounds(219, 7, 98, 23);
+				panel2.add(rDisco);
 			}
 			{
-				rCilindrohueco = new JRadioButton("Targeta Madre");
-				rCilindrohueco.setSelected(true);
-				rCilindrohueco.addActionListener(new ActionListener() {
+				rTarjetaMadre = new JRadioButton("Tarjeta Madre");
+				rTarjetaMadre.setSelected(true);
+				rTarjetaMadre.addActionListener(new ActionListener() {
 					public void actionPerformed(ActionEvent e) {
-						panel5.setVisible(true);
-						panel4.setVisible(false);
-						panel3.setVisible(false);
-						rEsfera.setSelected(false);
-						rCilindro.setSelected(false);
-						rCilindrohueco.setSelected(true);	
+						panelMadre.setVisible(true);
+						panelDisco.setVisible(false);
+						panelRam.setVisible(false);
+						panelProcesador.setVisible(false);
+		                
+						rRam.setSelected(false);
+						rDisco.setSelected(false);
+						rTarjetaMadre.setSelected(true);
+						rProcesador.setSelected(false);
 					}
 				});
-				rCilindrohueco.setBounds(6, 7, 118, 23);
-				panel2.add(rCilindrohueco);
+				rTarjetaMadre.setBounds(6, 7, 118, 23);
+				panel2.add(rTarjetaMadre);
 			}
 			
-			JRadioButton rdbtnProcesador = new JRadioButton("Procesador");
-			rdbtnProcesador.setBounds(318, 7, 98, 23);
-			panel2.add(rdbtnProcesador);
+			rProcesador = new JRadioButton("Procesador");
+			rProcesador.addActionListener(new ActionListener() {
+				public void actionPerformed(ActionEvent e) {
+					panelDisco.setVisible(false);
+					panelRam.setVisible(false);
+					panelMadre.setVisible(false);
+					panelProcesador.setVisible(true);
+					rRam.setSelected(false);
+					rDisco.setSelected(false);
+					rTarjetaMadre.setSelected(false);
+					rProcesador.setSelected(true);
+				}
+			});
+			rProcesador.setBounds(318, 7, 98, 23);
+			panel2.add(rProcesador);
 		}
 		{
-			panel3 = new JPanel();
-			panel3.setBorder(new TitledBorder(null, "", TitledBorder.LEADING, TitledBorder.TOP, null, null));
-			panel3.setBounds(6, 229, 422, 119);
-			contentPanel.add(panel3);
-			panel3.setLayout(null);
+			panelRam = new JPanel();
+			panelRam.setBorder(new TitledBorder(null, "", TitledBorder.LEADING, TitledBorder.TOP, null, null));
+			panelRam.setBounds(6, 229, 508, 119);
+			contentPanel.add(panelRam);
+			panelRam.setLayout(null);
+			
+			JLabel lblNewLabel_3 = new JLabel("Cantidad de Memoria");
+			lblNewLabel_3.setBounds(206, 11, 150, 14);
+			panelRam.add(lblNewLabel_3);
+			
+			JLabel lblNewLabel_2 = new JLabel("Tipo Memoria");
+			lblNewLabel_2.setBounds(10, 71, 106, 14);
+			panelRam.add(lblNewLabel_2);
 			{
-				JLabel lblNewLabel = new JLabel("Radio Esfera");
+				JLabel lblNewLabel = new JLabel("Marca");
 				lblNewLabel.setBounds(10, 11, 103, 14);
-				panel3.add(lblNewLabel);
+				panelRam.add(lblNewLabel);
 			}
+			
+			comboBoxCantidadMemoria = new JComboBox();
+			comboBoxCantidadMemoria.setModel(new DefaultComboBoxModel(new String[] {"<<Seleccionar>>", "8GB", "16GB"}));
+			comboBoxCantidadMemoria.setBounds(206, 36, 136, 20);
+			panelRam.add(comboBoxCantidadMemoria);
+			
+			textFieldTipoMemoria = new JTextField();
+			textFieldTipoMemoria.setBounds(10, 88, 136, 20);
+			panelRam.add(textFieldTipoMemoria);
+			textFieldTipoMemoria.setColumns(10);
 			{
-				RadioEsfera = new JTextField();
-				RadioEsfera.setBounds(10, 36, 136, 20);
-				panel3.add(RadioEsfera);
-				RadioEsfera.setColumns(10);
+				TextFieldMarca = new JTextField();
+				TextFieldMarca.setBounds(10, 36, 136, 20);
+				panelRam.add(TextFieldMarca);
+				TextFieldMarca.setColumns(10);
 			}
 		}
-		/*
-		 protected float precio;
-protected int cantidadDisponible;
-protected int numeroserie;
-protected int cantidadminima;
-Proveedor proveedor;
-		 */
+		
+		panelProcesador = new JPanel();
+		panelProcesador.setBorder(new TitledBorder(null, "", TitledBorder.LEADING, TitledBorder.TOP, null, null));
+		panelProcesador.setBounds(6, 229, 508, 119);
+		contentPanel.add(panelProcesador);
+		panelProcesador.setLayout(null);
+		
+		JLabel lblNewLabel_4 = new JLabel("Marca");
+		lblNewLabel_4.setBounds(10, 11, 110, 14);
+		panelProcesador.add(lblNewLabel_4);
+		
+		textFieldMarca1 = new JTextField();
+		textFieldMarca1.setBounds(10, 36, 136, 20);
+		panelProcesador.add(textFieldMarca1);
+		textFieldMarca1.setColumns(10);
+		
+		JLabel lblNewLabel_5 = new JLabel("Modelo");
+		lblNewLabel_5.setBounds(206, 11, 102, 14);
+		panelProcesador.add(lblNewLabel_5);
+		
+		textFieldModelo2 = new JTextField();
+		textFieldModelo2.setBounds(206, 36, 136, 20);
+		panelProcesador.add(textFieldModelo2);
+		textFieldModelo2.setColumns(10);
+		
+		JLabel lblNewLabel_6 = new JLabel("Tipo de conector");
+		lblNewLabel_6.setBounds(10, 71, 106, 14);
+		panelProcesador.add(lblNewLabel_6);
+		
+		textFieldTipoconetor = new JTextField();
+		textFieldTipoconetor.setBounds(10, 88, 136, 20);
+		panelProcesador.add(textFieldTipoconetor);
+		textFieldTipoconetor.setColumns(10);
+		
+		JLabel lblNewLabel_7 = new JLabel("Velocidad");
+		lblNewLabel_7.setBounds(206, 71, 106, 14);
+		panelProcesador.add(lblNewLabel_7);
+		
+		cbxVelocidad = new JComboBox();
+		cbxVelocidad.setModel(new DefaultComboBoxModel(new String[] {"<<Seleccionar>>", "2,4GHz", "3,0GHz", "3,30GHz"}));
+		cbxVelocidad.setBounds(206, 88, 136, 20);
+		panelProcesador.add(cbxVelocidad);
 		{
 			JPanel buttonPane = new JPanel();
 			buttonPane.setLayout(new FlowLayout(FlowLayout.RIGHT));
@@ -300,27 +404,28 @@ Proveedor proveedor;
 					public void actionPerformed(ActionEvent e) {
 						if(validarCampos()) {
 						Componente compo = null;
+						String nombre = textFieldNombreComponente.getText();
 						int numeroserie = Tienda.getInstance().getGeneradorcodigoCompo();
 						int cantidad = Integer.valueOf(spncantidad.getValue().toString());
 						float precio = Float.valueOf(spnprecio.getValue().toString());
 						int cantidadmininima = Integer.valueOf(spnMinima.getValue().toString());
 						
-						/*
-						if(rEsfera.isSelected()) {
-							compo = new Esfera(precio,unitario,codigo,cant,Float.valueOf(RadioEsfera.getText()));
+						
+						if(rTarjetaMadre.isSelected()) {
+							compo = new TarjetaMadre(nombre, precio, cantidadmininima, cantidadmininima, cantidadmininima, null, "", "", "", "", null);
 						}
 						
-						if(rCilindro.isSelected()) {
+						/**if(rCilindro.isSelected()) {
 							compo =  new Cilindro(Base,unitario,codigo,cant,Float.valueOf(Longitud.getText()),Float.valueOf(RadioCilindro.getText()));
 						}
 						
 						if(rCilindrohueco.isSelected()) {
 							compo = new Cilindrohueco(Base,unitario,codigo,cant,Float.valueOf(Longitud2.getText()),Float.valueOf(RadioCilindrohueco.getText()),Float.valueOf(RadioInterior.getText()));
 						}
-						
-						Tienda.getInstance().intrduc(compo);
+						**///
+						//Tienda.getInstance().intrduc(compo);
 						JOptionPane.showMessageDialog(null, "Se Introdujo correctamente el Componente ");
-						limpiarVentana();*/
+						limpiarVentana();
 						}else{
 							JOptionPane.showMessageDialog(null, "Completar campos para poder fabricar  los Quesos ");
 						}
@@ -347,12 +452,12 @@ Proveedor proveedor;
 	}
 	public void limpiarVentana() {
 	
-		RadioCilindro.setText("");
-		RadioInterior.setText("");
-		Longitud.setText("");
-		RadioEsfera.setText("");
-		RadioCilindrohueco.setText("");
-		Longitud2.setText("");
+		textFieldMarca.setText("");
+		textfieldModelo.setText("");
+		textFieldModelo.setText("");
+		TextFieldMarca.setText("");
+		textfieldMarca.setText("");
+		textfieldTipoconector.setText("");
 		
 		spnprecio.setValue(0);
 		spncantidad.setValue(0);
